4712c97f6a90a9b2b5b76d84a13ee757f3f28b8d
Carlos <carlitosxd2006@gmail.com>
2025-10-27 10:52:25 -0600

    cambiando fondo

--- ActionIA/ActionIA/obj/Debug/net8.0-android/ActionIA.AssemblyInfo.cs
+++ ActionIA/ActionIA/obj/Debug/net8.0-android/ActionIA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActionIA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+698c0321832162bcdbfd51fcb361ec3daf569896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+77c64f9a3f47a7fa5c7a17303a694cb4a0c5f09e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActionIA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActionIA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ActionIA/ActionIA/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ActionIA.AssemblyInfo.cs
+++ ActionIA/ActionIA/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/ActionIA.AssemblyInfo.cs
@@ -11,14 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "com.companyname.actionia")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "ActionIA")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActionIA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+698c0321832162bcdbfd51fcb361ec3daf569896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c64f9a3f47a7fa5c7a17303a694cb4a0c5f09e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActionIA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActionIA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
